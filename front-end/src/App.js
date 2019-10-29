2083f46853d6a600ba0936252c30f6c2c39fe0e9
Jabril Breckenridge <jabriltb2@gmail.com>
2019-10-28 19:57:50 -0500

    commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,31 +1,19 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import SignupForm from "./components/Onboard";
-<<<<<<< HEAD
-import TasksList from "./components/todolists/TasksList";
-import Login from "./components/Login";
-import EventsList from "./components/EventsList";
-=======
 import Login from './components/Login';
 import EventsTasks from './components/EventsTasks';
 import EventsList from './components/EventsList';
->>>>>>> ee01f842835db0d795d564b9ae682414f6d9c12d
 import "./App.css";
 import TheHeader from './components/Header'
 
 function App() {
 	return (
 		<div className="App">
-<<<<<<< HEAD
-			<Route path="/taskslist" exact component={TasksList} />
-			<PrivateRoute path="/taskslist/:id" render={props => <TasksList id={props.value.match.params.event_id} />} />
-			<PrivateRoute path="/eventslist" component={EventsList} />
-=======
 
 			<TheHeader />
 			<PrivateRoute path='/eventslist' component={EventsList} />
 			<PrivateRoute path='/eventstasks' component={EventsTasks} />
->>>>>>> ee01f842835db0d795d564b9ae682414f6d9c12d
 			<Route path="/register" component={SignupForm} />
 			<Route path="/login" component={Login} />
 		</div>
--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,48 +1,31 @@
-import React from 'react';
-
+import React from "react";
 
 function TheHeader() {
+	return (
+		<div>
+			<section className="banner">
+				<div className="banner-container">
+					<h1>Corporate Event Planner</h1>
 
-    return (
-        
-        <div>
-        <section class='banner'>
-          
-           <div class='banner-container'>
-          
-           <h1>Corporate Event Planner</h1>
-          
-           <p>Events with intelligence. Handling the stress so your event is a success!</p>
-               
-          
-           </div>
-       
-       </section>
-        
-        
-        
-        <header className='nav-container'>
-               
-               <div class='head-logo'>
-               
-               <h2> Corp E - Planner</h2>
-               
-               </div>
-               
-               <div className='navbar'>
-                   <a href='https://corpeventplanner.netlify.com/'>Home</a>                   
-                   <a href='https://corpeventplanner.netlify.com/about-page.html'>About Us</a>
-                   <a href='#'>Contact Us</a>
-                   <a href='/register'>Sign up</a>
-                   <a href='/login'>Log in</a>
-               </div>
+					<p>Events with intelligence. Handling the stress so your event is a success!</p>
+				</div>
+			</section>
 
-           </header>
+			<header className="nav-container">
+				<div className="head-logo">
+					<h2> Corp E - Planner</h2>
+				</div>
 
-           </div>
-        
-    )
-
+				<div className="navbar">
+					<a href="https://corpeventplanner.netlify.com/">Home</a>
+					<a href="https://corpeventplanner.netlify.com/about-page.html">About Us</a>
+					<a href="#">Contact Us</a>
+					<a href="/register">Sign up</a>
+					<a href="/login">Log in</a>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default TheHeader;
